89e0daf21fcdd65a93e0e9fabddddfa3663eeb0f
PhalgunNK <59506768+PhalgunNK@users.noreply.github.com>
2020-01-04 09:08:08 -0500

    Add files via upload

--- N_Queens.java
+++ N_Queens.java
@@ -1,37 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package n_queens;
-
+package RandomRestartMain;
 /**
  *
- * @author kulkarni
+ * @author Phalgun kulkarni
  */
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class N_Queens {
+private static final int Runtimes = 500;
 
-    private static final int RUNTEST = 500;
-
-    public N_Queens() {
-
-    }
-
+public N_Queens() {}
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-
-            System.out.println("Enter the value for N in this n-Queen problem: ");
-            int NODE_SIZE= scan.nextInt();
-            //Take the input size for the n queen problem
-            Node.setSize(NODE_SIZE);
+        System.out.println("Enter the value for N in this n-Queen problem: "); //Takeing input from the user for board configuaration(Number of queens)
+        int NODE_SIZE= scan.nextInt();
+             Node.setSize(NODE_SIZE);
             N_Queens board = new N_Queens();
-            System.out.println("Test for " + NODE_SIZE + " Queens problem : ");
-            //Initializing all the variables
-            double SM_SUM_Successes = 0, SM_AVE_Successes = 0, SM_Success_Steps=0,
+            System.out.println("Generating for " + NODE_SIZE + " board configuration : "); // displaying the size of the board configuration
+
+            double SM_SUM_Successes = 0, SM_AVE_Successes = 0, SM_Success_Steps=0,    //Initializing the variables
                     SM_Avg_Success_Steps=0, SM_Fail_Steps = 0, SM_Avg_Fail_Steps = 0 ;
             double SA_Sum_Successes = 0, SA_AVE_Success = 0, SA_Success_Steps = 0,
                     SA_Avg_Success_Steps = 0, SA_Fail_Steps = 0, SA_Avg_Fail_Steps = 0;
@@ -40,70 +28,97 @@ public class N_Queens {
             double RRS_Sum_Successes = 0, RRS_Ave_Successes = 0,
                     RRS_Success_Steps = 0, RRS_Avg_Success_Steps=0,
                     RandomRestartSideways_Count=0;
-          for (int currentTest = 1; currentTest <= RUNTEST; currentTest++) {
 
-                Queen[] initialBoard = board.generateBoard();
-                            HillClimb hclimb
-                        = new HillClimb(initialBoard);
-                                     
-                Node HillClimbnode =hclimb.climbingAlgo();
-                                
-                if (HillClimbnode.getH() == 0) {
-                    SA_Sum_Successes++;
-                    SA_Success_Steps =hclimb.getSteps();
-                    SA_Avg_Success_Steps=SA_Avg_Success_Steps+SA_Success_Steps;
-                }
-                else
-                {
-                    SA_Fail_Steps=hclimb.getSteps();
-                    SA_Avg_Fail_Steps=SA_Avg_Fail_Steps+SA_Fail_Steps;
+            for (int currentTest = 1; currentTest <= Runtimes; currentTest++) {
+               Queen[] initialBoard = board.generateChessBoard();
+                RandomRestart randomRestart                            
+                        = new RandomRestart(initialBoard);           //Calling RandomRestart class
+
+                
+
+                SidewaysMove sidewaysMove                            //Calling Sidewas move class   
+                        = new SidewaysMove(initialBoard);
+
+                RandomRestartSideways randomRestartSideways          //Calling RandomRestart with sidewaysmove class
+                        = new RandomRestartSideways(initialBoard);
+
+                Node randomRestartNode = randomRestart.climbingAlgo();
+                Node sidewaysMoveNode = sidewaysMove.climbingAlgo();
+                Node randomRestartSidewaysNode = randomRestartSideways.climbingAlgo();
+
+               
+                if (randomRestartNode.getH() == 0) {                 //Random Restart without sideways move
+                    RR_Sum_Successes++;
+                    RR_Success_Steps=randomRestart.getStepCount();
+                    RR_Avg_Success_Steps=RR_Avg_Success_Steps+RR_Success_Steps;
+                    RandomRestart_Count=RandomRestart_Count+(randomRestart.getRandomRestartUsed());
                 }
 
-                if(currentTest==33)
-                {
-                    System.out.println("First Path for Hill Climbing" );
-                    ArrayList<Node> x =hclimb.listToPrint();
-                   hclimb.printPath(x);
+
+                if (randomRestartSidewaysNode.getH() == 0) {          //Random Restart with sideways move
+                    RRS_Sum_Successes++;
+                    RRS_Success_Steps=randomRestartSideways.getStepCount();
+                    RRS_Avg_Success_Steps=RRS_Avg_Success_Steps+RRS_Success_Steps;
+                    RandomRestartSideways_Count=RandomRestartSideways_Count+randomRestartSideways.getRandomRestartSidewaysUsed();
+                }
+ 
+                if (sidewaysMoveNode.getH() == 0) {
+                    SM_SUM_Successes++;
+                    SM_Success_Steps=sidewaysMove.getSSteps();
+                    SM_Avg_Success_Steps=SM_Avg_Success_Steps+SM_Success_Steps;
+
+                }
+                else{
+                    SM_Fail_Steps=sidewaysMove.getSSteps();
+                    SM_Avg_Fail_Steps=SM_Avg_Fail_Steps+SM_Fail_Steps;
+                }
+                if(currentTest==376) {                                          // Genrating Queen board and Path and Path cost
+                    System.out.println("Third Path for Sideways Move");
+                    ArrayList<Node> x = sidewaysMove.getNodesToPrint();
+                    sidewaysMove.printPath(x);
                     System.out.println("Path cost: " + x.size());
                 }
-                if(currentTest==97){
-                    System.out.println("Second Path for Hill Climbing" );
-                    ArrayList<Node> x =hclimb.listToPrint();
-                   hclimb.printPath(x);
+
+                if(currentTest==181)  {
+                    System.out.println("First Path for Sideways Move");
+                    ArrayList<Node> x = sidewaysMove.getNodesToPrint();
+                    sidewaysMove.printPath(x);
                     System.out.println("Path cost: " + x.size());
                 }
-               if(currentTest==139) {
-                    System.out.println("Second Path for Hill Climbing");
-                    ArrayList<Node> x =hclimb.listToPrint();
-                   hclimb.printPath(x);
+                if(currentTest==214) {
+                    System.out.println("Second Path for Sideways Move");
+                    ArrayList<Node> x = sidewaysMove.getNodesToPrint();
+                    sidewaysMove.printPath(x);
                     System.out.println("Path cost: " + x.size());
 
                 }
-        }
-           //Calculating Average
-            SA_AVE_Success =
-                    SA_Sum_Successes / RUNTEST;
+            }
+
+            SA_AVE_Success =                                       //Calculating Average
+            SA_Sum_Successes / Runtimes;
 
             SM_AVE_Successes =
-                    SM_SUM_Successes / RUNTEST;
+            SM_SUM_Successes / Runtimes;
 
             RR_AVE_Successes =
-                    RR_Sum_Successes / RUNTEST;
+            RR_Sum_Successes / Runtimes;
 
             RRS_Ave_Successes =
-                RRS_Sum_Successes / RUNTEST;
-          System.out.println(
-                    "Out of " + RUNTEST + " trails\n"
-                    + " Success Count = " + SA_Sum_Successes
-                    + " \n Success rate = " + SA_AVE_Success 
-                    + " \n Fail count = " + (500.0 - SA_Sum_Successes)
-                    + " \n Failure rate = " + (1 - SA_AVE_Success)
-                    + " \n Avg Success Steps : " + ((SA_Avg_Success_Steps)/(SA_Sum_Successes))
-                    + " \n Avg Fail Steps : " + ((SA_Avg_Fail_Steps)/(500-SA_Sum_Successes)));
-               }
+            RRS_Sum_Successes / Runtimes;
 
-        //Generate a Chess Board with randomly placing the Queens on the Board
-    public Queen[] generateBoard(){
+   
+
+            System.out.println("Random Restart without Sideways moves:"
+            + " \nAvrage number of Steps without sideways move  = " + ((RR_Avg_Success_Steps)/500)
+            + " \nAvrage number of Random Restarts without sideways move  =" + (RandomRestart_Count/500));
+
+        System.out.println("Random Restart Sideways :"
+            + " \nAvrage number of Steps with sideways move = " + ((RRS_Avg_Success_Steps)/500)
+            + " \nAvrage number of Random Restarts with sideways move = " + (RandomRestartSideways_Count)/500);
+        }
+
+       
+    public Queen[] generateChessBoard(){          //Generating a Board with randomly placing the Queens on the Board
         Queen[] start = new Queen[Node.getSize()];
         Random gen = new Random();
 
--- Node.java
+++ Node.java
@@ -1,22 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package n_queens;
-
+package RandomRestartMain;
 /**
  *
  * @author kulkarni
  */
 import java.util.ArrayList;
-
 public class Node implements Comparable<Node>{
-
-    static  int size = 8; //Initializing the size
+    static  int size = 8;  //Initializing the size
      Queen[] state;
-     ArrayList<Node> nextNode;  //Storing all the nextNode in an ArrayList of Node class
-     int h; //Heuristic value stored and checked for zero
+     ArrayList<Node> nextNode;  //Storing all the nextNode
+     int h; //Heuristic value 
 
     public Node() {
         state = new Queen[size];
@@ -32,23 +24,15 @@ public class Node implements Comparable<Node>{
         }
         h = 0;
     }
-
-
-    //setters and getters for setting and getting the size of the n-queen problems from the user input
-    static public int getSize() {
+    static public int getSize() {             // setting and getting the size of the n-queen problems from the inout provided by the user.
         return size;
     }
-
     static public void setSize(int Size) {
         size = Size;
     }
 
-
-    //Generating all the possible nextNode of the node passed as an argument to this method
-    public ArrayList<Node> createNode(Node startState){
-
+    public ArrayList<Node> createNode(Node startState){     //Generating all the possible nextNodes
         int count=0;
-
         for (int i = 0; i < size; i++) {
             for (int j = 1; j < size; j++) {
                 nextNode.add(count, new Node(startState));
@@ -57,35 +41,24 @@ public class Node implements Comparable<Node>{
                 count++;
             }
         }
-
         return nextNode;
     }
 
-
-
-
-    //Computing the h is done by this method
-    //Heuristic value is computed by calculating the number of queens that can attack each other by calling the canAttack boolean
-    //method that returns a boolean value based on the configuration of Queens.
     public int calculateH(){
-
-        for (int i = 0; i < size - 1; i++){
-            for (int j = i + 1; j < size; j++){
-                if (state[i].checkAttack(state[j])){
-                    h++;
+    for (int i = 0; i < size - 1; i++){
+        for (int j = i + 1; j < size; j++){
+            if (state[i].checkAttack(state[j])){
+                h++;
                 }
             }
         }
-
         return h;
     }
 
-    //Getter for the h
-    public int getH(){
+    public int getH(){  //Get method for the hueristic
 
         return this.h;
     }
-
     public int compareTo(Node n){
         if (this.h < n.getH()) {
             return -1;
@@ -101,7 +74,6 @@ public class Node implements Comparable<Node>{
             state[i]= new Queen(s[i].getRows(), s[i].getColumns());
         }
     }
-
 
     public String toString(){
 
